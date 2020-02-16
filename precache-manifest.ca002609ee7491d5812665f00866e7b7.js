6d89ad66337d3ee7db97809fe68087c2f963d514
Mohammad Bahaa <MohammadBahaa_2001mb@outlook.com>
2020-02-16 23:11:11 +0200

    Updates

---
+++ precache-manifest.ca002609ee7491d5812665f00866e7b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3675a8b663b4790879af05ccf408f2d",
+    "url": "/react-cloudy/index.html"
+  },
+  {
+    "revision": "f52aabceb24e6f352e94",
+    "url": "/react-cloudy/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "69092338c8c2c4f57e1c",
+    "url": "/react-cloudy/static/css/main.01980345.chunk.css"
+  },
+  {
+    "revision": "f52aabceb24e6f352e94",
+    "url": "/react-cloudy/static/js/2.729b96ff.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-cloudy/static/js/2.729b96ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69092338c8c2c4f57e1c",
+    "url": "/react-cloudy/static/js/main.15601e94.chunk.js"
+  },
+  {
+    "revision": "5ab1c378ff6b448a7b71",
+    "url": "/react-cloudy/static/js/runtime-main.f812686d.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-cloudy/static/media/bg.b6796c73.jpg"
+  }
+]);
